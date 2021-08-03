79fddacdcb35b18f140ad16522881d500aca960e
Julie J <jeannet.julie@gmail.com>
2021-08-03 14:50:13 +0200

    Utiliser reduce() pour créer un objet à partir d'un tableau. J'adore !

---
+++ reduce/js/transports.js
@@ -0,0 +1,20 @@
+// Utiliser reduce() pour créer un objet à partir d'un tableau
+
+// imaginons un tableau simple des modes de transports utilisés  par des invités
+const transports = ["voiture", "vélo", "marche", "voiture", "tram", "voiture", "marche", "moto"];
+// on souhaite pouvoir créer un objet qui fasse le décompte du nombre de personnes
+// qui viennent en voiture, en vélo, en tram...
+// l'utilisation de reduce() fait le job. J'adore !
+
+const transportObjet = transports.reduce(
+    (acc, current) => {
+
+        if (!acc[current]) {
+            acc[current] = 0;
+        } acc[current] += 1;        
+        return acc;
+        
+    }, {}
+);
+
+console.log("tableau des transports", transportObjet);
