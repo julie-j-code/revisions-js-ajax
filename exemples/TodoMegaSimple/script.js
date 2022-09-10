b4cfdf825926b1ce5aea60abd6c8ccf55230bca3
Julie J <jeannet.julie@gmail.com>
2022-09-10 20:11:07 +0200

    Ajout de categories

--- exemples/TodoMegaSimple/script.js
+++ exemples/TodoMegaSimple/script.js
@@ -1,4 +1,3 @@
-const todoinput = document.querySelector("#todoinput");
 const addbutton = document.querySelector("#add");
 const todolist = document.querySelector("#todolist");
 
@@ -11,11 +10,12 @@ addbutton.addEventListener("click", addToList);
 function addToList(event) {
     event.preventDefault();
     valueInput = todoinput.value;
+    categorie=categories.value;
     console.log(valueInput)
     // dès qu'on a récupéré la valeur, on crée notre paragraphe
     let todoItem = document.createElement("p")
     todoItem.classList.add('todo-item');
-    todoItem.innerHTML = `${valueInput} <button class="delete" onclick="deleteItem(event)"> x </button>`;
+    todoItem.innerHTML = `${valueInput} - Category : ${categorie} <button class="delete" onclick="deleteItem(event)"> x </button>`;
     // puis on l'accroche à la liste existante
     todolist.appendChild(todoItem);
     todoinput.value = ""
