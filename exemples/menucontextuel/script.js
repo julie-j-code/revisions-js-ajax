fef57b2beebc1f3e096144b067ee9adae2b8bb95
Julie J <jeannet.julie@gmail.com>
2022-09-13 10:39:20 +0200

    Ajout de conditions pour contraindre les positions du menu contextuel à ne dépasser des valeurs max

--- exemples/menucontextuel/script.js
+++ exemples/menucontextuel/script.js
@@ -5,12 +5,21 @@ window.onload = () => {
     document.addEventListener("contextmenu", (event) => {
         event.preventDefault();
         menu.style.display = "block";
+        // console.log(menu.clientWidth);
 
         const posX = event.clientX;
         const posY = event.clientY;
 
-        menu.style.top = posY + "px";
-        menu.style.left = posX + "px";
+        // faut calculer la position la plus à droite du menu pour qu'il ne soit pas coupé
+        // equivalent de la largeur de la fenêtre - largeur du menu avec une marge additionnelle de 5px à droite
+        const maxX = window.innerWidth - menu.clientWidth;
+        // equivalent de la hauteur de la fenêtre - hauteur du menu 
+        const maxY = window.innerHeight - menu.clientHeight;
+        posX < maxX ? menu.style.left = posX + "px" : menu.style.left = maxX;
+        posY < maxY ? menu.style.top = posY + "px" : menu.style.top = maxY;
+
+        // menu.style.top = posY + "px";
+        // menu.style.left = posX + "px";
     })
 
     document.addEventListener("click", () => {
