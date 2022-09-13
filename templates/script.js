34f03d942fc291c6042ece659acfe2c47f1cd022
Julie J <jeannet.julie@gmail.com>
2022-09-13 18:31:14 +0200

    Création d'un composant web qui utilise notre modèle comme contenu

--- templates/script.js
+++ templates/script.js
@@ -1,4 +1,13 @@
-// on rappelle que template reste invisible côté html tant qu'aucune référence n'y est faite dans le code
-let template = document.getElementById('my-paragraph');
-let templateContent = template.content;
-document.body.appendChild(templateContent);
+// Les modèles fonctionnent mieux avec des composants web. Créons un composant web qui utilise notre modèle comme contenu. Nous l'appellerons <my-paragraph> :
+
+customElements.define('my-paragraph',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            let template = document.getElementById('my-paragraph');
+            let templateContent = template.content;
+            document.body.appendChild(templateContent.cloneNode(true));
+        }
+    })
+
+// Le point important à noter est que l'on ajoute un clone du contenu du modèle à la racine du DOM, créé à l'aide de la méthode Node.cloneNode().
