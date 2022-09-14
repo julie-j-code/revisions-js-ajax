678ea576d8febbefbe2f8c87c3969d796a17064f
Julie J <jeannet.julie@gmail.com>
2022-09-14 18:22:50 +0200

    Ajout deuxième paramètre pour l'id du template à utiliser

--- templates/function/index.js
+++ templates/function/index.js
@@ -1,14 +1,16 @@
 const myButton = document.getElementById('myButton');
 
-const showTemplate = (x) => {
-    const myTemp = document.getElementById("article");
+const showTemplate = (x,y) => {
+    // x pour l'élement dans lequel on souhaite injecté le html
+    // y pour l'identifiant du template qu'on souhaite utiliser
+    const myTemp = document.getElementById(y);
     const elementFromTemplate = myTemp.outerHTML.replaceAll("template", "div");
     const myElement = document.querySelectorAll(x)[0];
     console.log("myElement", myElement);
     myElement.innerHTML = elementFromTemplate;
 }
 
-showTemplate("main");
+showTemplate("main","article");
 
 
 
