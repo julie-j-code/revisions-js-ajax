5c5c9b4fbb75dd28cec35c3b3807f092c6e5334c
Julie J <jeannet.julie@gmail.com>
2022-09-16 15:37:36 +0200

    Injection du fragment template dans un élément du DOM, notre customElement ou whateverElement. A ce niveau, ça fonctionne très bien.

--- templates/webComponent/components/ColContainer.js
+++ templates/webComponent/components/ColContainer.js
@@ -2,19 +2,22 @@
 // Custom Element <col-container>
 export default class SecondTimer extends HTMLElement {
     constructor(){
-        // super invoque le constructeur du parent
         super()   
-        // this.onload=alert('J\'Existe !') 
         this.setAttribute("class","row")  
         const col=document.querySelectorAll('col-container div')
         col.forEach((element)=> element.classList.add("col-6"))
         
     }
-
-
-
-
-    
-
 }
+
+// Alternative : notre fragment existant côté html, 
+// On va le chercher, indépendamment de tout Custom Element :
+const template=document.getElementById("myTemplate")
+// On l'injecte dans le DOM du document actif (ici le deuxième div.container)
+const destination=document.querySelector("#destination")
+destination.appendChild(template.content)
+// console.log("destination",destination);
+// destination.innerHTML=template.content
+
+
 
