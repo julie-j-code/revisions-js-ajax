ff8a10f4a03bda5673e5aaee99a4f94c674b4238
Julie J <jeannet.julie@gmail.com>
2022-09-16 17:36:41 +0200

    Experience du Shadow Dom... peut-être un truc prometteur

--- templates/webComponent/components/ColContainer.js
+++ templates/webComponent/components/ColContainer.js
@@ -19,5 +19,26 @@ destination.appendChild(template.content)
 // console.log("destination",destination);
 // destination.innerHTML=template.content
 
+// trosième grand pilier : le Shadow Dom
+// les éléments qu'on y ajoute (comme des styles par exemple) seront totalement isolés. 
+// ils seront insérés à partir de root.
+
+const root= document.querySelector('#root');
+console.log(root);
+root.attachShadow({mode : "open"})
+
+const text = document.createElement('p')
+text.textContent="Je suis issu du shadow pour l'exemple"
+// ça me semble très religieusement incorrecte, mais essayons
+const style = document.createElement('style')
+style.textContent = `p {color : red}`
+text.appendChild(style)
+
+// au passage, on notera que append permet d'insérer plusieurs éléments d'un coup
+root.shadowRoot.append(text,style)
+
+
+
+
 
 
