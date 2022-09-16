6b5ae6f823d47e6173af60d8524c13b354cdb487
Julie J <jeannet.julie@gmail.com>
2022-09-16 09:20:50 +0200

    Morcellement du script pour importer SecondTimer  dans mon point d'entrée app.js

---
+++ templates/webComponent/app.js
@@ -0,0 +1,16 @@
+import SecondTimer from "./components/SecondTimer.js";
+
+customElements.define('second-timer', SecondTimer);
+
+const addBtn = document.querySelector("#add")
+const addNewInput = () => {
+    const newInput = document.createElement('input')
+    newInput.classList.add("form-control")
+    newInput.classList.add("mb-2")
+    const myForm = document.getElementById("myForm")
+    myForm.appendChild(newInput)
+    // et pour tester le setAttribute et l'obervattion du changement
+    document.getElementsByTagName("second-timer")[0].setAttribute("mycustomattribute","Nouveau champ détecté !")
+}
+addBtn.addEventListener("click", addNewInput)
+
--- templates/webComponent/components/SecondTimer.js
+++ templates/webComponent/components/SecondTimer.js
@@ -1,4 +1,4 @@
-class SecondTimer extends HTMLElement {
+export default class SecondTimer extends HTMLElement {
 
     // par défaut, tous les attributs ne sont pas observables, mais on va pouvoir en déclarer certains comme l'étant pour utiliser un fonction de callback intéressante qui réagit à la modifiction des attributs
 
@@ -47,20 +47,3 @@ class SecondTimer extends HTMLElement {
     }
 
 }
-
-// pour l'instancier en quelque sorte, on le définit comme instace de la classe SecondTimer, identifiable comme élément <second-timer> :
-customElements.define('second-timer', SecondTimer);
-
-
-const addBtn = document.querySelector("#add")
-const addNewInput = () => {
-    const newInput = document.createElement('input')
-    newInput.classList.add("form-control")
-    newInput.classList.add("mb-2")
-    const myForm = document.getElementById("myForm")
-    myForm.appendChild(newInput)
-    // et pour tester le setAttribute et l'obervattion du changement
-    document.getElementsByTagName("second-timer")[0].setAttribute("mycustomattribute","Nouveau champ détecté !")
-}
-addBtn.addEventListener("click", addNewInput)
-
