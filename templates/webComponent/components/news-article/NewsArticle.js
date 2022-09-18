fa4bd1136fe051257791534ddf07a7d2eb7b281d
Julie J <jeannet.julie@gmail.com>
2022-09-18 14:07:38 +0200

    On consomme du jsonplaceholder avant d'injecter le tout dans le même <template> qu'on clone

--- templates/webComponent/components/news-article/NewsArticle.js
+++ templates/webComponent/components/news-article/NewsArticle.js
@@ -1,37 +1,37 @@
-
 export default class NewsArticle extends HTMLElement {
+
 
     constructor() {
         super()
-        // On récupère le template souhaité finalement la seule alternative aux backsticks (mais c'est selon moi totalement exclu de recourir à <template>
         const template = document.getElementById("newsarticle")
-        // On clone le template pour rappel
-        const clone = document.importNode(template.content, true);
-        // on se crée des getter
-        this.baliseH1 = clone.querySelectorAll("h1");
-        this.baliseH2 = clone.querySelectorAll("h2");
-        this.baliseP = clone.querySelector("p");
 
 
-        this.appendChild(clone)
-
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(
+                response => response.json()
+            )
+            .then(
+                posts => {
+                    for (let post of posts) {
+                        // On clone le template pour rappel
+                        const clone = document.importNode(template.content, true);
+                        const baliseH2 = clone.querySelector("h2");
+                        baliseH2.textContent = post.title
+                        const baliseP = clone.querySelector("p");
+                        baliseP.innerHTML = post.body
+                        this.appendChild(clone)
+                    }
+                }
+            )
     }
+
+
+
 
     connectedCallback() {
-        console.log("coucou")
-        console.log(this)
-        console.log(this.baliseH1);
-        console.log(this.baliseH2);
-        console.log(this.baliseP);
-        // ça marche très bien. ça fait penser au OnInit() d'Angular
-        for(let h2 of this.baliseH2){h2.textContent=`C'est moi là encore qui décide `}
-        for(let h1 of this.baliseH1){h1.textContent=`C'est moi qui décide `}
+        console.log(`coucou je viens d'être rattaché au DOM !`)
+
     }
 
+
 }
-
-
-
-
-
-
