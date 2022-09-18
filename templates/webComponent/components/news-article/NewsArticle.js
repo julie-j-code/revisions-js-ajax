82132f6dfb57e5e655d8e954952bf1490100e602
Julie J <jeannet.julie@gmail.com>
2022-09-18 10:26:57 +0200

    Une utilisation intéressante de <template> sans Shadow Dom avec getters et clone.querySelectorAll()

--- templates/webComponent/components/news-article/NewsArticle.js
+++ templates/webComponent/components/news-article/NewsArticle.js
@@ -3,17 +3,29 @@ export default class NewsArticle extends HTMLElement {
 
     constructor() {
         super()
+        // On récupère le template souhaité finalement la seule alternative aux backsticks (mais c'est selon moi totalement exclu de recourir à <template>
+        const template = document.getElementById("newsarticle")
+        // On clone le template pour rappel
+        const clone = document.importNode(template.content, true);
+        // on se crée des getter
+        this.baliseH1 = clone.querySelectorAll("h1");
+        this.baliseH2 = clone.querySelectorAll("h2");
+        this.baliseP = clone.querySelector("p");
 
-        // le chemin est relatif à l'endroit où le fragment sera injecté
-        let file = "./components/news-article/news-article.html"
 
-        fetch(file)
-            .then(x => x.text())
-            .then(y => {
-                this.attachShadow({ mode: 'open' })
-                this.shadowRoot.innerHTML = y
-            }); 
+        this.appendChild(clone)
 
+    }
+
+    connectedCallback() {
+        console.log("coucou")
+        console.log(this)
+        console.log(this.baliseH1);
+        console.log(this.baliseH2);
+        console.log(this.baliseP);
+        // ça marche très bien. ça fait penser au OnInit() d'Angular
+        for(let h2 of this.baliseH2){h2.textContent=`C'est moi là encore qui décide `}
+        for(let h1 of this.baliseH1){h1.textContent=`C'est moi qui décide `}
     }
 
 }
