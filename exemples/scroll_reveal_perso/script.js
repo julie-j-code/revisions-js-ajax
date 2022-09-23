6e084680b72cae16ae0ae16f14653d0aac2146fe
Julie J <jeannet.julie@gmail.com>
2022-09-23 17:50:30 +0200

    Refacto pour pouvoir passer au scroll une fonction qui prenne en paramètre le resutlat d'un querySelector (NodeList ou Element unique) et la fonction que je souhaite voir appliquée

--- exemples/scroll_reveal_perso/script.js
+++ exemples/scroll_reveal_perso/script.js
@@ -1,59 +1,34 @@
-const divElement = document.querySelectorAll('DIV')
-
-// Différence entre la hauteur de viewport (fenêtre) et hauteur du document ? 
-
-// 1 --------- sont rigoureusement équivalents, et correspondent bien à la hauteur de la fenêtre = viewport
-console.log("innerHeight", window.innerHeight);
-console.log("document clientHeight", document.documentElement.clientHeight);
-// ---------- beaucoup plus grand, correspond bien à la hauteur du document, plus précisément du body
-console.log(document.body.clientHeight);
-
-// Différence entre la position du haut d'un élément et le début du viewport. Pour la gestion des animations au sroll, c'est ce qui nous intéressera
-console.log('getBoundingClientRect', document.querySelector("#myDiv5").getBoundingClientRect().top)
-
-// Ces valeurs sensiblement différentes l'une de l'autre tiennent belle et bien compte de la distance qui sépare le haut de l'élément du début de body. Elles ne sont donc  pas relatives au  viewport. 
-console.log(document.querySelector("#myDiv5").offsetTop)
-console.log(document.querySelector("#myDiv5").offsetY)
-
-// on peut déduire de ce qui précède le calcul du moment où un élément rentre dans notre champ de vision. dépasse la ligne de flottaison. c'est le moment où le haut de l'élément est  positionné à une distance du haut du viewport correspondant à la hauteur du viewport
-// cependant, si on s'en tenait à cette "intersection", les animations seraient déclenchées beaucoup trop top : à l'instant où l'élément concerné rentre dans le champ de vision. 
-// comme on souhaite retarder cet instant  pour par exemple attendre que l'élément arrive au milieu de l'écran, on peut utiliser window.innerHeight*0.5 comme référence
-
-if(document.querySelector("#myDiv5").getBoundingClientRect().top<window.innerHeight-(window.innerHeight*0.5)){console.log("bingo !")}
-
-
-// exemple de fonction de callback qui se contentera de rendre visible des éléments invisibles
-const show = (x) => { 
+// exemple de fonction de callback qui se contentera de rendre visible des éléments invisibles avec pour faire jojo un peu de jeu d'opacité
+const show = (x) => {
     x.style.visibility = "visible"
     setTimeout(
-        x.style.opacity=100
-    ),2000
- }
+        x.style.opacity = 100
+    ), 2000
+}
 
-// on vérifie si ça marche au global : ok
-// window.addEventListener("load", () => {
-//     divElement.forEach((x)=>show(x))
-// })
+const myFunction = (querySelected, functionToApply) => {
+    console.log(Object.getPrototypeOf(querySelected).constructor.name == "NodeList");
 
-// Pour simplifier dans un premier temps, je teste la fonction sans lui passer de paramètre, en faisant en sorte qu'elle s'applique à mon divElement initialisé en haut de page.
-// Pour simplifier dans un premier temps, je teste la fonction sans lui passer de paramètre, en faisant en sorte qu'elle s'applique à mon divElement initialisé en haut de page.
-window.addEventListener("scroll", () => {
+    // on ne sait pas si on se retrouve avec un élément unique ou un tableau (NodeList) 
+    // on envisage donc le cas d'une collection d'une suiite d'élements (NodeList)
+    if (Object.getPrototypeOf(querySelected).constructor.name == "NodeList") {
+        querySelected.forEach(elem => {
 
-    // on ne sait pas si on se retrouve avec un élément ou une collection 
-    // on envisage donc le cas d'une collection d'éléments
-    if (Object.getPrototypeOf(divElement).constructor.name == "NodeList") {
-        divElement.forEach(elem => {
-            
-            if(elem.getBoundingClientRect().top<window.innerHeight-(window.innerHeight*0.5)){
-                show(elem)
+            if (elem.getBoundingClientRect().top < window.innerHeight - (window.innerHeight * 0.5)) {
+                functionToApply(elem)
             }
         })
     }
     // sinon c'est que c'est un élément unique qui se verra appliqué la fonction sitôt qu'il franchit 50% de la hauteur du viewport
     else {
-        if(elem.getBoundingClientRect().top<window.innerHeight-(window.innerHeight*0.5)){
-            show(elem)
+        if (querySelected.getBoundingClientRect().top < window.innerHeight - (window.innerHeight * 0.5)) {
+            functionToApply(querySelected)
         }
     }
 
-})
+}
+
+// Je teste maintenant en passant un élément en paramètre, puis la fonction que je souhaite voir appliquée
+
+const divElement = document.querySelectorAll('DIV')
+window.addEventListener("scroll", () => myFunction(divElement, show))
