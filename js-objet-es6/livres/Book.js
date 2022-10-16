5d988b93ef241200bb1a7d24cce097e0044a2e25
Julie J <jeannet.julie@gmail.com>
2022-10-16 07:26:44 +0200

    Tout en un ou intégration des fonctions utiles initialement importées à la seule classe Book en mode static. Simple exercice. Cf Kanban projet pour déclinaisons plus intéressantes des méthodes statiques

--- js-objet-es6/livres/Book.js
+++ js-objet-es6/livres/Book.js
@@ -1,23 +1,23 @@
 // exemples simples et didactiques
 export default class Book {
     static totalBooks = 0
-    static allBooks=[]
+    static allBooks = []
 
-    constructor(title, pages, notation="") {
+    constructor(title, pages, notation = "") {
         this.title = title
         this.pages = pages
         this.notation = notation;
         this.page = 1
         // faire en sorte qu'à chaque fois qu'un livre est instancié, la quantité de livres se mette à jour ainsi que le tableau des books
-        Book.totalBooks+=1;
-        Book.allBooks = [{title, pages, notation},...Book.allBooks]
+        Book.totalBooks += 1;
+        Book.allBooks = [{ title, pages, notation }, ...Book.allBooks]
     }
 
     getPage = () => {
         console.log(this.page);
     }
 
-    nextPage () {
+    nextPage() {
         if (this.page < this.pages) {
             this.page++
             console.log("après incrementation", this.page);
@@ -25,10 +25,24 @@ export default class Book {
     }
 
     // on doit pouvoir réinitialiser la page à la fermeture
-    close(){
-        this.page=1
+    close() {
+        this.page = 1
     }
 
+    static findByWord(txt) {
+        txt=txt.toLowerCase()
+        const result = Book.allBooks.filter(b => b.title.toLowerCase().includes(txt))
+        console.log("Résultat de recherche", result);
+        return result;
+    }
+
+    static evaluate(bookId, note) {
+        if (bookId && bookId > 0 && note != null) {
+            console.log("Evaluation ok");
+            console.log(Book.allBooks[bookId]);
+            Book.allBooks[bookId].notation = note
+        }
+    }
 
 
 
--- js-objet-es6/livres/scripts.js
+++ js-objet-es6/livres/scripts.js
@@ -1,5 +1,5 @@
 import Book from "./Book.js";
-import {notation} from "./Notations.js";
+// import {notation} from "./Notations.js";
 
 let firstBook = new Book("Guide des films", 2200)
 console.log(Book.allBooks)
@@ -10,8 +10,13 @@ let thirdBook = new Book("Guide des océans", 2200)
 let fourstBook = new Book("Guide des lacs", 2200)
 console.log(Book.allBooks)
 
-notation(3,"C'est formidable !!!", Book.allBooks);
+Book.evaluate(3,"C'est formidable !!!", Book.allBooks);
 console.log("après notation", Book.allBooks[3]);
+
+Book.evaluate(2,"Très instructif ", Book.allBooks);
+console.log("après notation", Book.allBooks[2]);
+
+Book.findByWord("Océan")
 
 console.log(Book.totalBooks);
 
